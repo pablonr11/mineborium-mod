f51895dc709fbbe93c0c7e1d8ecf9f76aa365ede
Pablo <pablonr11@gmail.com>
2023-06-24 15:15:33 +0200

    Initial commit

---
+++ src/main/java/com/mundobachata/mineborium/Mineborium.java
@@ -0,0 +1,34 @@
+package com.mundobachata.mineborium;
+
+import com.mojang.logging.LogUtils;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.CreativeModeTabEvent;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.slf4j.Logger;
+
+@Mod(Mineborium.MOD_ID)
+public class Mineborium {
+    public static final String MOD_ID = "mineborium";
+
+    private static final Logger LOGGER = LogUtils.getLogger();
+
+    public Mineborium() {
+        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        modEventBus.addListener(this::commonSetup);
+        MinecraftForge.EVENT_BUS.register(this);
+
+        modEventBus.addListener(this::addCreative);
+    }
+
+    private void commonSetup(final FMLCommonSetupEvent event) {
+
+    }
+
+    private void addCreative(CreativeModeTabEvent.BuildContents event) {
+
+    }
+}
