344a03bd01bd74f249618edd2e5c2fc660fe5843
Pablo <pablonr11@gmail.com>
2023-06-29 19:18:03 +0200

    Delete waterlog from Weathering Marlborium

--- src/main/java/com/mundobachata/mineborium/block/custom/WeatheringMarlboriumFullBlock.java
+++ src/main/java/com/mundobachata/mineborium/block/custom/WeatheringMarlboriumFullBlock.java
@@ -4,8 +4,11 @@ import net.minecraft.core.BlockPos;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.util.RandomSource;
 import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.level.LevelAccessor;
 import net.minecraft.world.level.block.LeavesBlock;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.material.Fluid;
+import net.minecraft.world.level.material.FluidState;
 import net.minecraft.world.phys.shapes.Shapes;
 import net.minecraft.world.phys.shapes.VoxelShape;
 
@@ -40,5 +43,15 @@ public class WeatheringMarlboriumFullBlock extends LeavesBlock implements Weathe
     @Override
     public WeatherState getAge() {
         return this.weatherState;
+    }
+
+    @Override
+    public boolean placeLiquid(LevelAccessor levelAccessor, BlockPos blockPos, BlockState blockState, FluidState fluidState) {
+        return false;
+    }
+
+    @Override
+    public boolean canPlaceLiquid(BlockGetter blockGetter, BlockPos blockPos, BlockState blockState, Fluid fluid) {
+        return false;
     }
 }
