c4c49f87bdbfae728612ef43a4fcdeac779c6d02
Pablo <pablonr11@gmail.com>
2023-06-30 21:40:20 +0200

    Added compact marlborium block recipe

--- src/main/java/com/mundobachata/mineborium/datagen/ModRecipeProvider.java
+++ src/main/java/com/mundobachata/mineborium/datagen/ModRecipeProvider.java
@@ -1,9 +1,12 @@
 package com.mundobachata.mineborium.datagen;
 
+import com.mundobachata.mineborium.Mineborium;
+import com.mundobachata.mineborium.block.ModBlocks;
 import com.mundobachata.mineborium.item.ModItems;
 import net.minecraft.advancements.critereon.ItemPredicate;
 import net.minecraft.data.PackOutput;
 import net.minecraft.data.recipes.*;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.Items;
 import net.minecraft.world.level.ItemLike;
 import net.minecraftforge.common.crafting.conditions.IConditionBuilder;
@@ -39,8 +42,12 @@ public class ModRecipeProvider extends RecipeProvider implements IConditionBuild
                         .of(ModItems.MARLBORIUM.get()).build()))
                 .save(consumer);
 
-        twoByTwoPacker(consumer, RecipeCategory.MISC, ModItems.MARLBORIUM.get(), ModItems.MARLBORIUM_NUGGET.get());
+        twoByTwoPacker(consumer, RecipeCategory.MISC, ModItems.MARLBORIUM.get(),
+                ModItems.MARLBORIUM_NUGGET.get());
         oneToOneConversionRecipe(consumer, ModItems.ROLLING_PAPER.get(), Items.PAPER, "rolling_paper", 4);
+        nineBlockStorageRecipes(consumer, RecipeCategory.MISC, ModItems.MARLBORIUM.get(),
+                RecipeCategory.MISC, ModBlocks.MARLBORIUM_BLOCK.get(), "marlborium2",
+                null, "compact_marlborium_block", null);
     }
 
     protected static void twoByTwoPacker(Consumer<FinishedRecipe> consumer, RecipeCategory recipeCategory, ItemLike itemLike, ItemLike itemLike1) {
@@ -49,5 +56,13 @@ public class ModRecipeProvider extends RecipeProvider implements IConditionBuild
 
     protected static void oneToOneConversionRecipe(Consumer<FinishedRecipe> consumer, ItemLike crafted, ItemLike materials, @Nullable String group, int quantity) {
         ShapelessRecipeBuilder.shapeless(RecipeCategory.MISC, crafted, quantity).requires(materials).group(group).unlockedBy(getHasName(materials), has(materials)).save(consumer, getConversionRecipeName(crafted, materials));
+    }
+
+    protected static void nineBlockStorageRecipes(Consumer<FinishedRecipe> consumer, RecipeCategory recipeCategory1,
+                                                  ItemLike required, RecipeCategory recipeCategory2,
+                                                  ItemLike crafted, String resource2,
+                                                  @Nullable String group2, String resource1, @Nullable String group1) {
+        ShapelessRecipeBuilder.shapeless(recipeCategory1, required, 9).requires(crafted).group(group1).unlockedBy(getHasName(crafted), has(crafted)).save(consumer, new ResourceLocation(Mineborium.MOD_ID, resource1));
+        ShapedRecipeBuilder.shaped(recipeCategory2, crafted).define('#', required).pattern("###").pattern("###").pattern("###").group(group2).unlockedBy(getHasName(required), has(required)).save(consumer, new ResourceLocation(Mineborium.MOD_ID, resource2));
     }
 }
