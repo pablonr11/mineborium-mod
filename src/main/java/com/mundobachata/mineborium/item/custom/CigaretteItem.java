55f94ef1bed674535d80c753909725930244f276
Pablo <pablonr11@gmail.com>
2023-08-01 22:30:56 +0200

    Added creative mode check on CigaretteItem#use

--- src/main/java/com/mundobachata/mineborium/item/custom/CigaretteItem.java
+++ src/main/java/com/mundobachata/mineborium/item/custom/CigaretteItem.java
@@ -3,22 +3,20 @@ package com.mundobachata.mineborium.item.custom;
 import com.mundobachata.mineborium.entity.renderer.ModItemAnimations;
 import com.mundobachata.mineborium.networking.ModNetworking;
 import com.mundobachata.mineborium.networking.packet.SmokeC2SPacket;
-import net.minecraft.core.Direction;
 import net.minecraft.core.particles.ParticleTypes;
 import net.minecraft.sounds.SoundEvent;
 import net.minecraft.sounds.SoundEvents;
 import net.minecraft.sounds.SoundSource;
-import net.minecraft.util.RandomSource;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResultHolder;
 import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.ai.attributes.Attributes;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.food.FoodProperties;
 import net.minecraft.world.item.*;
 import net.minecraft.world.level.Level;
-import net.minecraft.world.phys.Vec3;
 import net.minecraftforge.client.extensions.common.IClientItemExtensions;
 
 import java.util.function.Consumer;
@@ -41,15 +39,17 @@ public class CigaretteItem extends Item {
     public InteractionResultHolder<ItemStack> use(Level level, Player player, InteractionHand hand) {
         ItemStack itemStack = player.getItemInHand(hand);
 
-        if(!playerHasLighter(player, itemStack)) {
-            return InteractionResultHolder.fail(itemStack);
-        }
+        if(!player.isCreative()) {
+            if(!playerHasLighter(player, itemStack)) {
+                return InteractionResultHolder.fail(itemStack);
+            }
 
-        if(!level.isClientSide) {
-            damageLighter(player, itemStack);
-        }
+            if(!level.isClientSide) {
+                damageLighter(player, itemStack);
+            }
 
-        player.getCooldowns().addCooldown(this, 84);
+            player.getCooldowns().addCooldown(this, 84);
+        }
 
         if(player.canEat(itemStack.getFoodProperties(player).canAlwaysEat())) {
             player.startUsingItem(hand);
